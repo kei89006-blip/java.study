8da17c70a3dc83b08920ba1270d101d63eeaabe3
keigo <kei89006@gmail.com>
2026-06-18 20:13:57 +0900

    Add files via upload

---
+++ Util.java
@@ -0,0 +1,10 @@
+class Util {
+    static double average(double a, double b) {
+        return (a + b) / 2.0;
+    }
+
+    public static void main(String args[]) {
+        double a = average(16.0, 22.8);
+        System.out.println("Average value is " + a); 
+    }
+}
